fb667f4f3c3d907c9e9829b5873e0eb451700466
Phydon <84594612+Phydon@users.noreply.github.com>
2021-06-06 13:19:26 +0200

    Add files via upload

--- InputOutputFromExternalFile.py
+++ InputOutputFromExternalFile.py
@@ -2,7 +2,39 @@
   
 import sys
 # For getting input from input.txt file
-sys.stdin = open('input.txt', 'r') 
+sys.stdin = open('Input.txt', 'r') 
   
 # Printing the Output to output.txt file
-sys.stdout = open('output.txt', 'w')
+sys.stdout = open('Output.txt', 'w')
+
+#####################################Implementation########################################
+import sys
+
+sys.stdin = open('C:/Users/Hubi/Desktop/Phydon/InOut/Input/InputText.txt', 'r')
+sys.stdout = open('C:/Users/Hubi/Desktop/Phydon/InOut/Output/OutputText.txt', 'w')
+############################Int
+def get_ints(): 
+    return map(int, sys.stdin.readline().strip().split())
+
+a,b,c,d = get_ints()
+print(a, b, c, d)
+###########################List
+def get_ints(): 
+    return list(map(int, sys.stdin.readline().strip().split()))
+ 
+Arr = get_ints()
+print(Arr)
+###########################String
+def get_string(): 
+    return sys.stdin.readline().strip()
+ 
+string = get_string()
+print(string)
+"""
+example to read multiple lines:
+i = 0
+n = 3
+for i in range(n): 
+    string = get_string()
+    print(string)
+""" 
