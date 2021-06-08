79f44bb8e9becbdeaa04b8b3028ca7a3cb710fb9
Phydon <84594612+Phydon@users.noreply.github.com>
2021-06-08 17:55:43 +0200

    Add files via upload

--- RandomTestCaseGenerator.py
+++ RandomTestCaseGenerator.py
@@ -8,8 +8,9 @@ sys.stdout = open('pythonProjects\CompetitiveProgramming\Input.txt', 'w')
 # maxv = int(1e9) 
 ############### - - END - - ###############
 
-n = int(randrange(0, 10, 1))
-maxv = int(100)
+# n = int(10) # fixed number
+n = int(randrange(0, 10, 1)) # random number from 0 to 10 in steps 1
+maxv = int(100) # maximum generated numbers
 
 print(n) 
 print(" ".join([str(randrange(maxv)) for _ in range(n)])) 
