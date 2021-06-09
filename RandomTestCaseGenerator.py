fa40f7854c994587f3b09cdd5e1aee38189a11b7
Phydon <84594612+Phydon@users.noreply.github.com>
2021-06-09 20:10:32 +0200

    Add files via upload

--- RandomTestCaseGenerator.py
+++ RandomTestCaseGenerator.py
@@ -3,13 +3,8 @@ from random import *
 
 sys.stdout = open('pythonProjects\CompetitiveProgramming\Input.txt', 'w')
 
-###### - - GENERATE HUGE NUMBERS - - ######
-# n = int(1e5) 
-# maxv = int(1e9) 
-############### - - END - - ###############
-
 # n = int(10) # fixed number
-n = int(randrange(0, 10, 1)) # random number from 0 to 10 in steps 1
+n = int(randrange(1, 10, 1)) # random number from 1 to 10 in steps = 1
 maxv = int(100) # maximum generated numbers
 
 print(n) 
--- Template.py
+++ Template.py
@@ -1,16 +1,29 @@
-import sys, time
-import math, cmath
+import sys
+import math, cmath, time, functools
 
 sys.stdin = open('pythonProjects\CompetitiveProgramming\Input.txt', 'r')
 sys.stdout = open('pythonProjects\CompetitiveProgramming\Output.txt', 'w')
 
 def get_time(func):
-    def wrapper():
+    def time_wrapper():
         start = time.perf_counter()
         func()
         end = time.perf_counter()
         print("\nTime taken:", end - start)
-    return wrapper
+    return time_wrapper
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f'{k} = {v!r}' for k, v in kwargs.items()]
+        signature = ', '.join(args_repr + kwargs_repr)
+        print(f'Calling {func.__name__}({signature})')
+        value = func(*args, **kwargs)
+        print(f'{func.__name__!r} returned {value!r}')
+        return value
+    return wrapper_debug
+
 
 def get_int():
     return int(sys.stdin.readline().strip())
@@ -29,8 +42,8 @@ def get_string():
 def solve():
 ##### - - - CODE STARTS HERE - - - #####
 
+   
     
-
 
 
 ##### - - - CODE ENDS HERE - - - #####
